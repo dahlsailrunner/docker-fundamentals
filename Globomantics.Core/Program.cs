96e4d915c9ee37cb1a4f52677c73a8e80562000f
Erik Dahl <dahlsailrunner@outlook.com>
2020-12-23 15:29:10 -0600

    updates to containerize the UI

--- Globomantics.Core/Program.cs
+++ Globomantics.Core/Program.cs
@@ -20,7 +20,7 @@ namespace Globomantics.Core
                         .ReadFrom.Configuration(context.Configuration)
                         .Enrich.FromLogContext()
                         .WriteTo.Console()
-                        .WriteTo.Seq("http://localhost:5341"); ;
+                        .WriteTo.Seq("http://host.docker.internal:5341"); ;
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
